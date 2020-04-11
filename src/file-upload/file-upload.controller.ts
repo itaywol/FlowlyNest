18c2b59af6110068c2210e7a882e1da61012073d
Itay Wolfish <itay@linnovate.net>
2020-04-11 16:25:45 +0300

    added basic file upload

--- src/file-upload/file-upload.controller.ts
+++ src/file-upload/file-upload.controller.ts
@@ -2,33 +2,18 @@ import {
   Controller,
   Post,
   UseInterceptors,
-  UploadedFiles,
-  Body,
+  UploadedFile,
 } from '@nestjs/common';
-import { FilesInterceptor } from '@nestjs/platform-express';
-const MulterGoogleStorage = require('multer-google-storage');
+import { FileInterceptor } from '@nestjs/platform-express';
+import { File } from '@google-cloud/storage';
 
 //TODO: finish interfaces
 //TODO: find solution for multer-google-storage import
 @Controller('file')
 export class FileUploadController {
   @Post('upload')
-  @UseInterceptors(
-    FilesInterceptor('file', 1, {
-      storage: MulterGoogleStorage.storageEngine({
-        projectId: process.env.GOOGLE_PROJECT_ID,
-        keyFilename: 'src/constants/storage.json',
-        bucket: process.env.GOOGLE_STORAGE_BUCKET_NAME,
-        fileName: (req, file, cb) => {
-          const fileNamesplit = file.originalName.split('.');
-          const ext = fileNamesplit[fileNamesplit.length - 1];
-          cb(null, `${Date.now()}.${ext}`);
-        },
-      }),
-    }),
-  )
-  public async save(@UploadedFiles() file: any) {
-    console.log(file);
-    return '';
+  @UseInterceptors(FileInterceptor('file'))
+  public async save(@UploadedFile() file: any) {
+    return file;
   }
 }
