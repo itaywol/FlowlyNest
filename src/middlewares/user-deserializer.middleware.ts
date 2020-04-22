527e99bf817eb15cb552a86cd864f1b3a963d391
Michael Samoha <miki1882@gmail.com>
2020-04-21 19:36:51 -0700

    Removed leftovers

--- src/middlewares/user-deserializer.middleware.ts
+++ src/middlewares/user-deserializer.middleware.ts
@@ -1,12 +1,10 @@
-import { Injectable, NestMiddleware, NotFoundException, Inject } from '@nestjs/common';
+import { Injectable, NestMiddleware, NotFoundException } from '@nestjs/common';
 import { UserService } from 'user/user.service';
 import { RequestWithAuth } from 'user/interfaces/user.interface';
-import { Logger } from 'winston';
 
 @Injectable()
 export class UserDeserializerMiddleware implements NestMiddleware {
-  constructor(private userService: UserService,
-    @Inject('winston') private logger: Logger) {}
+  constructor(private userService: UserService) {}
 
   use = async (req: RequestWithAuth, _res: Response, next: () => void) => {
     if ((req.session as any).passport !== undefined) {
